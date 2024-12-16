033afb485dcb80b77fc580629b2b23776772fac5
diaszakir <mailforstudy84@gmail.com>
2024-12-16 19:22:31 +0500

    assignment2 task1

---
+++ Task1.kt
@@ -0,0 +1,24 @@
+package Assignment2
+
+// Псевдоним типов
+typealias Name = String
+
+fun main() {
+    var age: Int = 19;
+    var height: Double = 185.4;
+    var isStudent: Boolean = true;
+    val name: String = "Dias";
+
+    // Other data types
+    val b: Byte = 10;
+    val c: Short = 47;
+    var d: Long = 9213921414914L;
+    var e: Float = 2.1F;
+    var ch: Char = 'a';
+    println("Hello $name!");
+    var any_var: Any = 123;
+    // Null type
+    var f: String? = null;
+    // Arrays
+    var array_of_num: Array<Int> = arrayOf(1,2,3,4);
+}
